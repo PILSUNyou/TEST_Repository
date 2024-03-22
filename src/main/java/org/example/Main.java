381357e798b56bca1d6049a407c6bccd691e8ad1
PILSUNyou <dbyo1125@naver.com>
2024-03-23 00:24:19 +0900

    게시물 구현 및 리스트 미구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,46 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("안녕하세요.");
+        System.out.println("== 프로그램을 시작합니다 ! ==");
+        Scanner sc = new Scanner(System.in);
+        int lastPostId = 0;
+        while (true){
+            System.out.println("명령어 입력 : ");
+            String cmd = sc.nextLine();
+
+            if (cmd.length() == 0){
+                System.out.print("명령어를 입력해 주십시오.");
+                continue;
+            }
+            if (cmd.equals("exit")){
+                break;
+            }
+            else if(cmd.equals("post write")){
+                int id = lastPostId + 1;
+                lastPostId = id;
+                System.out.print("게시글의 제목을 입력해주세요 : ");
+                String title = sc.nextLine();
+                System.out.print("게시글의 내용을 입력해주세요 : ");
+                String body = sc.nextLine();
+
+                Post post = new Post(id, title, body);
+                System.out.println("게시물이 작성되었습니다.");
+            }
+            else if(cmd.equals("post list")){
+                System.out.println("게시물이 존재하지 않습니다.");
+            }
+
+            else{
+                System.out.println("잘못된 입력입니다.");
+                continue;
+            }
+        }
+        sc.close();
+        System.out.println("== 프로그램을 종료합니다 ! ==");
+
     }
 }
 
---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Post {
+    public int id;
+    public String title;
+    public String body;
+    public Post(int id, String title, String body){
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+}
